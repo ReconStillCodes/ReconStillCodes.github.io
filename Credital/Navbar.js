3c31341a30f7ddb089e19f3afd98e672beca4705
Matthew <155131940+ReconStillCodes@users.noreply.github.com>
2024-09-25 15:57:14 +0700

    Delete Credital directory

--- Credital/Navbar.js
+++
@@ -1,8 +0,0 @@
-let account_data = JSON.parse(localStorage.getItem("credital_account"))
-
-let navbar_name = document.getElementById("navbar-name")
-navbar_name.innerText = account_data[0]
-
-function openAccount(){
-    window.open('Account.html', '_self')
-}
